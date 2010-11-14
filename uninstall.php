01a8627efe3438d7c896124cd29b69d23d48a47b
nc6400 <nc6400@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-11-14 21:25:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/foliamaptool/trunk@311421 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- uninstall.php
+++ uninstall.php
@@ -3,4 +3,7 @@ if ( !defined('WP_UNINSTALL_PLUGIN') ) {
     exit();
 }
 delete_option('foliamaptool');
+
+//debug - remove when ready
+mail("cj@folia.dk","Foliamaptool deactivated",$_SERVER["SERVER_NAME"]);
 ?>
